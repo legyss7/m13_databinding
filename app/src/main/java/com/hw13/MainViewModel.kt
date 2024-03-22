40d1c9835d62b3a53cd8a8211cd3059264894003
Oleg <legyss@mail.ru>
2024-03-22 11:29:48 +0600

    modif MainViewModel and fragmeng_main

--- app/src/main/java/com/hw13/MainViewModel.kt
+++ app/src/main/java/com/hw13/MainViewModel.kt
@@ -2,8 +2,11 @@ package com.hw13
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.launch
 
 class MainViewModel(private val repository: MainRepository) : ViewModel() {
@@ -14,20 +17,34 @@ class MainViewModel(private val repository: MainRepository) : ViewModel() {
     private val _credential = MutableStateFlow(Credentials())
     val credentials = _credential.asStateFlow()
 
-    fun updateSearchText() {
+    private var searchJob: Job? = null
+    private var updateJob: Job? = null
+
+    init {
         viewModelScope.launch {
+            _credential.debounce(300).collect {
+                updateSearchText()
+            }
+        }
+    }
+
+    fun updateSearchText() {
+        updateJob?.cancel()
+        updateJob = viewModelScope.launch {
+            delay(300) // Задержка после вызова метода
             val searchText = credentials.value.textSearch
             if (searchText.length > 2) {
                 _state.value = State.Ready
+                searchData() // Вызов функции searchData() после задержки
             } else {
                 _state.value = State.Waiting
             }
-
         }
     }
 
     fun searchData() {
-        viewModelScope.launch {
+        searchJob?.cancel()
+        searchJob = viewModelScope.launch {
             _state.value = State.Search
             val searchText = credentials.value.textSearch
             val result = repository.getData(searchText)
